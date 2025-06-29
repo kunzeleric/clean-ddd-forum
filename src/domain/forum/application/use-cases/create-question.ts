df0b7221ed1be3fdd4bc2b16d23552d1198e9060
Eric Kunzel <kunzel.eric@gmail.com>
2025-06-29 10:05:39 -0300

    created new use cases `get-question-by-slug` and `create-question`

---
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -0,0 +1,33 @@
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { QuestionRepository } from '@/domain/forum/application/repositories/question-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+interface CreateQuestionUseCaseRequest {
+  authorId: string
+  title: string
+  content: string
+}
+
+interface CreateQuestionUseCaseResponse {
+  question: Question
+}
+
+export class CreateQuestionUseCase {
+  constructor(private questionRepository: QuestionRepository) {}
+
+  async execute({
+    authorId,
+    content,
+    title,
+  }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
+    const question = Question.create({
+      authorId: new UniqueEntityID(authorId),
+      title,
+      content,
+    })
+
+    await this.questionRepository.create(question)
+
+    return { question }
+  }
+}
